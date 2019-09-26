8dabb49492c9f6524b9c584da3a097927d1f3304
maendeleoperry <maendeleoperry@hotmail.com>
2019-09-26 19:43:51 +0000

    update2

--- Gemfile
+++ Gemfile
@@ -37,6 +37,8 @@ gem 'jbuilder', '~> 2.5'
 gem 'bootsnap', '>= 1.1.0', require: false
 
 group :development, :test do
+  gem 'rspec-rails', '~> 3.5'
+  gem "factory_bot_rails" 
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
 end
@@ -60,12 +62,6 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
-
-group :development, :test do
-  gem 'rspec-rails', '~> 3.5'
-  gem "factory_bot_rails" 
-end
-
 gem 'jquery-rails'
 gem 'bootstrap', '~> 4.3.1'
 gem 'simple_form'
--- spec/factories.rb
+++ spec/factories.rb
@@ -6,13 +6,13 @@ FactoryBot.define do
         sequence :email do |n|
             "dummyemail#{n}@gmail.com"
         end
-        password "secretPassword"
-        password_confirmation "secretPassword"
+        password  "secretPassword" 
+        password_confirmation  "secretPassword" 
     end
 
     factory :gram do
-        message "hello"
-        picture { fixture_file_upload(Rails.root.join('spec', 'fixtures', 'picture.png').to_s, 'image/png') }
+        message  "hello" 
+        picture  fixture_file_upload(Rails.root.join('spec', 'fixtures', 'picture.png').to_s, 'image/png') 
         association :user # calls the user factory
     end
 end
