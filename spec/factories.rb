4874be2563f30bc012ae60aebf7a0481d6ea9fa1
maendeleoperry <maendeleoperry@hotmail.com>
2019-09-26 19:53:11 +0000

    update3

--- spec/factories.rb
+++ spec/factories.rb
@@ -4,15 +4,15 @@ FactoryBot.define do
   end
     factory :user do
         sequence :email do |n|
-            "dummyemail#{n}@gmail.com"
+           { "dummyemail#{n}@gmail.com" }
         end
-        password  "secretPassword" 
-        password_confirmation  "secretPassword" 
+        password { "secretPassword" }
+        password_confirmation { "secretPassword" }
     end
 
     factory :gram do
-        message  "hello" 
-        picture  fixture_file_upload(Rails.root.join('spec', 'fixtures', 'picture.png').to_s, 'image/png') 
+        message { "hello" }
+        picture { fixture_file_upload(Rails.root.join('spec', 'fixtures', 'picture.png').to_s, 'image/png') }
         association :user # calls the user factory
     end
 end
